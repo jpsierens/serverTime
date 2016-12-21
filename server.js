efbfcec294b1e24c7f64968b5951c8fd77833582
jpsierens <jpsierens1988@gmail.com>
2016-12-21 10:02:34 -0600

    es6

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var http = require('http');
-var socketio = require('socket.io');
-var fs = require('fs');
-var html = fs.readFileSync('index.html', 'utf8');
+const http = require('http');
+const socketio = require('socket.io');
+const fs = require('fs');
+const html = fs.readFileSync('index.html', 'utf8');
 
-var app = http.createServer(function handler (req, res) {
+const app = http.createServer(function handler (req, res) {
     // HTTP server code always serves index.html file
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', Buffer.byteLength(html, 'utf8'));
@@ -11,11 +11,11 @@ var app = http.createServer(function handler (req, res) {
 }).listen(8080);
 
 // Upgrade regular HTTP server to Socket.IO server
-var io = socketio.listen(app);
+const io = socketio.listen(app);
 
 // every 1000 secs;
 setInterval(function tick () {
     // Send time to all connected sockets
-    var now = new Date().toUTCString();
+    const now = new Date().toUTCString();
     io.sockets.send(now);
 }, 1000);
